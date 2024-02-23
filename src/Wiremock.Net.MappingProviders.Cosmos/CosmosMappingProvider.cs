f86e6df3d33b3b6e942098ceffd2ce173d8f49b3
Rich Tamblyn <richard.tamblyn@puregym.com>
2024-02-23 16:53:36 +0000

    WIP Issues deserializing Interfaces in Mapping

--- src/Wiremock.Net.MappingProviders.Cosmos/CosmosMappingProvider.cs
+++ src/Wiremock.Net.MappingProviders.Cosmos/CosmosMappingProvider.cs
@@ -1,4 +1,6 @@
-﻿using WireMock;
+﻿using System.Net;
+using Microsoft.Azure.Cosmos;
+using WireMock;
 using WireMock.Owin.Mappers.Providers;
 
 namespace Wiremock.Net.MappingProviders.Cosmos;
@@ -9,9 +11,31 @@ internal class CosmosMappingProvider : IMappingProvider
 
     private CosmosMappingProviderOptions? _options;
 
+    private readonly Container _container;
+
+    private const string Database = "wiremock";
+    private const string Mappings = "mappings";
+
     public CosmosMappingProvider(CosmosMappingProviderOptions options)
     {
         _options = options;
+
+        var client = new CosmosClient(connectionString: options.ConnectionString);
+
+        var dbCreation = client.CreateDatabaseIfNotExistsAsync(id: Database).Result;
+        if (dbCreation.StatusCode != HttpStatusCode.Created && dbCreation.StatusCode != HttpStatusCode.OK)
+        {
+            throw new Exception("Cosmos Database could not be created");
+        }
+
+        var database = client.GetDatabase(Database);
+        var containerCreation = database.CreateContainerIfNotExistsAsync(new ContainerProperties(Mappings, "/id")).Result;
+        if (containerCreation.StatusCode != HttpStatusCode.Created && containerCreation.StatusCode != HttpStatusCode.OK)
+        {
+            throw new Exception("Cosmos Container could not be created");
+        }
+
+        _container = database.GetContainer(Mappings);
     }
 
     public KeyValuePair<Guid, IMapping>[] ToArray()
@@ -21,7 +45,16 @@ internal class CosmosMappingProvider : IMappingProvider
 
     public bool TryAdd(Guid key, IMapping mapping)
     {
-        throw new NotImplementedException();
+        var newMapping = Map(mapping);
+
+        var responseMessage = _container.CreateItemAsync(item: newMapping,
+                partitionKey: new PartitionKey(key.ToString()))
+            .Result;
+        return responseMessage.StatusCode switch
+        {
+            HttpStatusCode.Accepted => true,
+            _ => false
+        };
     }
 
     public bool TryRemove(Guid key, out bool result)
@@ -31,7 +64,14 @@ internal class CosmosMappingProvider : IMappingProvider
 
     public bool ContainsKey(Guid key)
     {
-        throw new NotImplementedException();
+        using var responseMessage = _container.ReadItemStreamAsync(
+            partitionKey: new PartitionKey(key.ToString()),
+            id: key.ToString()).Result;
+        return responseMessage.StatusCode switch
+        {
+            HttpStatusCode.NotFound => false,
+            _ => true
+        };
     }
 
     public void Update(Guid key, IMapping mapping)
@@ -41,5 +81,33 @@ internal class CosmosMappingProvider : IMappingProvider
 
     public int Count { get; }
 
-    ICollection<IMapping> IMappingProvider.Values => _values;
+    ICollection<IMapping> IMappingProvider.Values => ReturnValues();
+
+    private ICollection<IMapping> ReturnValues()
+    {
+        return new List<IMapping>();
+    }
+
+    private static CosmosMapping Map(IMapping mapping)
+    {
+        var mapped = new CosmosMapping(mapping.Guid,
+            mapping.UpdatedAt,
+            mapping.Title,
+            mapping.Description,
+            mapping.Path,
+            mapping.Settings,
+            mapping.RequestMatcher,
+            mapping.Provider,
+            mapping.Priority,
+            mapping.Scenario,
+            mapping.ExecutionConditionState,
+            mapping.NextState,
+            mapping.StateTimes,
+            mapping.Webhooks,
+            mapping.UseWebhooksFireAndForget,
+            mapping.TimeSettings,
+            mapping.Data);
+
+        return mapped;
+    }
 }
